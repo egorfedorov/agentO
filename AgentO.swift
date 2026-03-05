3c0e6d64be4ba2689388a39cc5a5752438f9347f
Egor Fedorov <egorfdrv@gmail.com>
2026-03-06 07:32:35 +0900

    chore: bump source version to 6.2.4

--- AgentO.swift
+++ AgentO.swift
@@ -2028,7 +2028,7 @@ struct BattleDuelContext {
 // MARK: - Main App Delegate
 
 class AgentODelegate: NSObject, NSApplicationDelegate, NSTextFieldDelegate {
-    static let sourceVersion = "6.2.3"
+    static let sourceVersion = "6.2.4"
     static func parseVersion(_ version: String) -> [Int] {
         return version
             .trimmingCharacters(in: .whitespacesAndNewlines)
